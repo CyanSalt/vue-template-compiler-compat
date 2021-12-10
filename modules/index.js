43cf66fba5e9dac37e7a1e125f0578f8d812c352
CyanSalt <c.salt@hotmail.com>
2021-12-10 17:17:30 +0800

    refactor: add composeModules

--- modules/index.js
+++ modules/index.js
@@ -1,9 +1,8 @@
+const { composeModules } = require('../utils')
 const model = require('./model')
 const slot = require('./slot')
 
-module.exports = {
-  preTransformNode(el, options) {
-    model.preTransformNode(el, options)
-    slot.preTransformNode(el, options)
-  },
-}
+module.exports = composeModules([
+  model,
+  slot,
+])
--- utils.js
+++ utils.js
@@ -16,7 +16,62 @@ function deleteAttr(el, name) {
   delete el.attrsMap[name]
 }
 
+function pluckModuleFunction(modules, key) {
+  return modules ? modules.map(m => m[key]).filter(fn => fn) : []
+}
+
+function composeModules(modules) {
+  const mod = {}
+  const preTransformNodeFns = pluckModuleFunction(modules, 'preTransformNode')
+  if (preTransformNodeFns.length) {
+    mod.preTransformNode = function (el, options) {
+      for (let i = 0; i < preTransformNodeFns.length; i++) {
+        el = preTransformNodeFns[i](el, options) || el
+      }
+      return el
+    }
+  }
+  const transformNodeFns = pluckModuleFunction(modules, 'transformNode')
+  if (transformNodeFns.length) {
+    mod.transformNode = function (el, options) {
+      for (let i = 0; i < transformNodeFns.length; i++) {
+        el = transformNodeFns[i](el, options) || el
+      }
+      return el
+    }
+  }
+  const postTransformNodeFns = pluckModuleFunction(modules, 'postTransformNode')
+  if (postTransformNodeFns.length) {
+    mod.postTransformNode = function (el, options) {
+      for (let i = 0; i < postTransformNodeFns.length; i++) {
+        postTransformNodeFns[i](el, options)
+      }
+    }
+  }
+  const transformCodeFns = pluckModuleFunction(modules, 'transformCode')
+  if (transformCodeFns.length) {
+    mod.transformCode = function (el, code) {
+      for (let i = 0; i < transformCodeFns.length; i++) {
+        code = transformCodeFns[i](el, code)
+      }
+      return code
+    }
+  }
+  const genDataFns = pluckModuleFunction(modules, 'genData')
+  if (genDataFns.length) {
+    mod.genData = function (el) {
+      let data = ''
+      for (let i = 0; i < genDataFns.length; i++) {
+        data += genDataFns[i](el)
+      }
+      return data
+    }
+  }
+  return mod
+}
+
 module.exports = {
   setAttr,
   deleteAttr,
+  composeModules,
 }
