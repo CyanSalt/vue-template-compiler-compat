c77e3ca9d2ad734834a0a659a984c2ddd99778f3
CyanSalt <c.salt@hotmail.com>
2022-02-17 18:37:17 +0800

    fix: esbuild iife

--- modules/syntax-esbuild.js
+++ modules/syntax-esbuild.js
@@ -3,7 +3,7 @@ const trimEnd = require('lodash.trimend')
 const transform = require('./syntax-transform')
 
 module.exports = transform(code => {
-  const result = esbuild.transformSync(`(function(){return ${code}})`, {
+  const result = esbuild.transformSync(`(function(){return ${code}})()`, {
     target: 'es2019',
   })
   return trimEnd(result.code, ';\n')
